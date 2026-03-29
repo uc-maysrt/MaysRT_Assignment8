fa310fc9b71ae0f8f136e9ba16397cc7042f763c
Robert Mays <10287611+uc-maysrt@users.noreply.github.com>
2026-03-29 19:21:11 -0400

    Blank initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MaysRT_Assignment8.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
